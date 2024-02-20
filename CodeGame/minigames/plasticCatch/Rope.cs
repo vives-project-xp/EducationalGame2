c8a076060da8a1c3785b90ae656145e668f32246
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-02-20 14:17:36 +0100

    rick roll probleem opgelost

--- CodeGame/minigames/plasticCatch/Rope.cs
+++ CodeGame/minigames/plasticCatch/Rope.cs
@@ -3,7 +3,7 @@ partial class Rope : TextureRect
 {
     public override void _Ready()
     {
-        Texture = GD.Load<Texture2D>("res://assets/Sea/line_plastic.png");
+        Texture = GD.Load<Texture2D>("res://assets/Sea/LinePlastic.png");
         float xSize = GetViewport().GetVisibleRect().Size.X;
         float ySize = GetViewport().GetVisibleRect().Size.Y;
         Position = new Vector2(xSize / 2, ySize / 8);
