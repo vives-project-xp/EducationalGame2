15166c14e1ae2a4b5ca45f45d5d299e7255ceabb
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-02-24 13:39:13 +0100

    scene tests

---
+++ CodeGame/World/Background.cs
@@ -0,0 +1,13 @@
+using Godot;
+partial class BGWorld : TextureRect
+{
+    public override void _Ready()
+    {
+        Texture = GD.Load<Texture2D>("res://Assets/World/Landen.png");
+        ExpandMode = ExpandModeEnum.IgnoreSize;
+    }
+    public override void _Process(double delta)
+    {
+        Size = GetViewportRect().Size;   
+    }
+}
