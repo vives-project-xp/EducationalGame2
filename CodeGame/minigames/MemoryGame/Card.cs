880f9a6d37e44c9c511296ea9201e45cc2dcbba4
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-02-26 12:12:32 +0100

    memory update

--- CodeGame/minigames/MemoryGame/Card.cs
+++ CodeGame/minigames/MemoryGame/Card.cs
@@ -2,10 +2,16 @@ using Godot;
 partial class Card : TextureButton
 {
     public Texture2D _TextureNormal;
+    public bool flipped { get; set;}
     //constructor 
     public Card(string TexturePath){
     
         _TextureNormal = ResourceLoader.Load<Texture2D>(TexturePath);
-        TextureNormal = ResourceLoader.Load<Texture2D>("res://assets/Industrial/blank.png");
+        TextureNormal = ResourceLoader.Load<Texture2D>("res://assets/Sea/Sea_backcard.png");
+    }
+
+    public override void _Pressed(){
+        TextureNormal = _TextureNormal;
+        flipped = true;
     }
 }
