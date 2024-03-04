a646177d4fdfffb202231a3397a1c170e4c88a7d
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-03-04 11:24:02 +0100

    memory game basic

--- CodeGame/minigames/MemoryGame/Card.cs
+++ CodeGame/minigames/MemoryGame/Card.cs
@@ -1,24 +1,28 @@
-using System.Diagnostics;
 using Godot;
 partial class Card : TextureButton
 {
-    public Texture2D _TextureNormal;
+    public Texture2D frontcard;
+    public Texture2D backcard;
     public bool done { get; set; }
     public bool flipped { get; set;}
     public bool selected { get; set; } = false;
     //constructor 
     public Card(string TexturePath){
     
-        _TextureNormal = ResourceLoader.Load<Texture2D>(TexturePath);
-        TextureNormal = ResourceLoader.Load<Texture2D>("res://assets/Sea/Sea_backcard.png");
+        frontcard = ResourceLoader.Load<Texture2D>(TexturePath);
+        backcard = ResourceLoader.Load<Texture2D>("res://assets/Sea/Sea_backcard.png");
+        IgnoreTextureSize = true;
+        StretchMode = StretchModeEnum.KeepAspectCentered;
+        CustomMinimumSize = new Vector2(250, 250);
+        
     }
 
     public override void _Pressed(){
-        TextureNormal = _TextureNormal;
+        TextureNormal = frontcard;
         selected = true;
         flipped = true;
     }
     public override void _Process(double delta){
-        TextureNormal = flipped ? _TextureNormal : ResourceLoader.Load<Texture2D>("res://assets/Sea/Sea_backcard.png");
+        TextureNormal = flipped ? frontcard : backcard;
     }
 }
