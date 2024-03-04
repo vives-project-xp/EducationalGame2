b9ef5e80c14f3de1625a6362340775d08d37f7ea
Colin Bossuyt <colin.bossuyt@gmail.com>
2024-03-04 15:51:40 +0100

    Functional Factory Wrecking minigame

---
+++ CodeGame/minigames/FactoryWrecking/FactoryPart7.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class FactoryPart7 : RigidBody2D, IFactoryPart
+{
+	public int Health  {get; set; } = 15;
+		[Export]
+    public NodePath HealthBarPath { get; set; }
+
+    private Healthbar HealthBar { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AddToGroup("FactoryPart");
+		HealthBar = (Healthbar)GetNode<ProgressBar>(HealthBarPath);
+		HealthBar.MaxValue = Health;
+		HealthBar.Value = Health;
+		GD.Print("Health: " + Health);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		AddToGroup("FactoryPart");
+	}
+
+	public void DecreaseHealth(int amount)
+	{
+		GD.Print("DecreaseHealth: " + amount);
+		Health -= amount;
+		HealthBar.Value = Health;
+		GD.Print("Health: " + Health);
+	}
+
+}
