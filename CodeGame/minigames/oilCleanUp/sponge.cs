7ec99c84eac1db0edfcc61fa3d140bfef9b80563
nikitavdc <114406785+nikitavdc@users.noreply.github.com>
2024-03-11 10:10:54 +0100

    oil cleanup game

---
+++ CodeGame/minigames/oilCleanUp/sponge.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Sponge : Sprite2D
+{
+        public override void _Ready()
+    {
+        Name = "Sponge";
+        Texture = GD.Load<Texture2D>("res://assets/Sea/Sea_sponge.png");
+        RegionEnabled = true;
+        RegionRect = new Rect2(5.5f, 5.75f, 606f, 706f);
+        Scale = new Vector2(0.1f, 0.1f);
+    }
+
+    public override void _PhysicsProcess(double delta) => Position = GetGlobalMousePosition() + new Vector2(0, 30);
+    
+}
