f2de21b43de426489f7e4f4f47e0e83a30728ce1
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-03-18 10:20:52 +0100

    file clean up

--- CodeGame/minigames/RestoreFarmland/RestoreFarmland.cs
+++ CodeGame/minigames/RestoreFarmland/RestoreFarmland.cs
@@ -12,3 +12,14 @@ public partial class RestoreFarmland : Node2D
 	{
 	}
 }
+
+
+public partial class Tiles : TileMap
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// cell size is 32x32
+		TileSet = ResourceLoader.Load("res://minigames/RestoreFarmland/TileSet.tres") as TileSet;
+	}
+}
