7e1bbb69cb8fb97882e3df27641a015de281542a
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-03-18 14:57:33 +0100

    potential fix of git files

--- CodeGame/World/Factoriesbutton.cs
+++ CodeGame/World/Factoriesbutton.cs
@@ -3,5 +3,4 @@ using Godot;
 public partial class Factoriesbutton : TextureButton
 {
 	public override void _Pressed() => PlayerHandler.ChangeScene(this, "res://minigames/FactoryWrecking/factoryWrecking.tscn");
-	
 }
