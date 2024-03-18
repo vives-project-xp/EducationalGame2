0857ff9a02f170e8fdfc97c3947d706c4eb4f3b0
nikitavdc <114406785+nikitavdc@users.noreply.github.com>
2024-03-18 16:20:35 +0100

    oil update

--- CodeGame/minigames/oilCleanUp/oilCleaning.cs
+++ CodeGame/minigames/oilCleanUp/oilCleaning.cs
@@ -1,12 +1,11 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class oilCleaning : Node2D
 {
 	private bool toClose= false;
 	private Sponge sponge { get; set; } = new();
-	float posX;
-	float posY;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,37 +13,26 @@ public partial class oilCleaning : Node2D
 		for (int i = 0; i < 10; i++)
 		{
 			Oil oil2 = new();
-			/*do{
-				AddChild(oil2);
-				foreach(var oils in GetTree().GetNodesInGroup("Oils"))
-				{
-					if(oils is Oil oil)
-					{
-						if(toClose==false)
-						{
-							//check for colosion with other oil
-							toClose = oil2.GetRect().Intersects(oil.GetRect());
-							
-						}
-					}
-					GD.Print(toClose);
-				}	
-				if(toClose==true)
-				{
-					RemoveChild(oil2);
-				}
-
-			} while(toClose==true);*/
 			AddChild(new Oil());
 		}
 		AddChild(new Sponge());
 		
 	}
 	
+	//check if sponge colides with oil
+	public void CheckCollision()
+	{
+		foreach (Oil oil in GetTree().GetNodesInGroup("Oil"))
+		{
+			Rect2 spongeRect = sponge.GetGlobalTransform() * sponge.GetRect();
+			Rect2 oilRect = oil.GetGlobalTransform() * oil.GetRect();
+			if (spongeRect.Intersects(oilRect))oil.QueueFree();
+		}
+	}
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		CheckCollision();
 	}
-
 }
