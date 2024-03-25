bbb96f3add1c245a9a011a6c85a22b168390bdcb
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-03-25 14:33:55 +0100

    refactoring code

--- CodeGame/World/PlasticMini.cs
+++ CodeGame/World/PlasticMini.cs
@@ -1,8 +1,33 @@
 using Godot;
 public partial class PlasticMini : TextureButton
 {
-    public override void _Pressed()
+  public override void _Ready()
+  {
+    SetPivotCenter();
+  }
+  public void SetPivotCenter()
+  {
+    Vector2 center = new(GetRect().Size.X / 2, GetRect().Size.Y / 2);
+    PivotOffset = center;
+  }
+  public override void _Pressed()
+  {
+    GetTree().ChangeSceneToFile("res://minigames/plasticCatch/PlasticCatch.tscn");
+  }
+  public override void _Input(InputEvent @event)
+  {
+    // check if mouse is on this button
+    if (@event is InputEventMouseMotion mouseMotion)
     {
-		GetTree().ChangeSceneToFile("res://minigames/plasticCatch/PlasticCatch.tscn");
+      if (GetGlobalRect().HasPoint(mouseMotion.Position))
+      {
+        Scale = new Vector2(1.4f, 1.4f);
+
+      }
+      else
+      {
+        Scale = new Vector2(1, 1);
+      }
     }
+  }
 }
