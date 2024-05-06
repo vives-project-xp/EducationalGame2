9a4dd66334713eee44cee8984596c6f2e8ec7aa7
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-05-06 09:04:12 +0200

    merge conflicts

--- ClimateChange/Scenes/Overlay/moregamesbtnoverlay.cs
+++ ClimateChange/Scenes/Overlay/moregamesbtnoverlay.cs
@@ -7,12 +7,25 @@ public partial class moregamesbtnoverlay : Button
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GetNode<RichTextLabel>("../moreGamesLabel").Visible = false;
+		GetNodeOrNull<RichTextLabel>("../moreGamesLabel").Visible = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(GetTree().CurrentScene.SceneFilePath == "res://Scenes/WorldMap/World.tscn")
+		{
+			GetNodeOrNull<RichTextLabel>("../moreGamesLabel").Visible = true;	
+		}else
+		{
+			GetNodeOrNull<RichTextLabel>("../moreGamesLabel").Visible = false;
+		}
+
 	}
 
 	public override void _Pressed()
 	{
-		var moreGamesLabel = GetNode<RichTextLabel>("../moreGamesLabel");
+		var moreGamesLabel = GetNodeOrNull<RichTextLabel>("../moreGamesLabel");
 		moreGamesLabel.Visible = !moreGamesLabel.Visible;
 	}
 
